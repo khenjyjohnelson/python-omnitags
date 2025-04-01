da22db579dfef2d9781d3773a649a0d6b0f3aecd
Khenjy Johnelson <71621764+khenjyjohnelson@users.noreply.github.com>
2025-04-01 13:32:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def main():
     config = read_config()
 
     # Example Outputs
-    print("Database Name:", config.Aliases.get("database", "Not Found"))
-    print("Table A1 Name:", config.Titles.get("tabel_a1_alias_v2", "Not Found"))
-    print("Upload Path for A1:", config.VUploadPath.get("tabel_a1", "Not Found"))
-    print("Flash Message for A1:", config.Views.get("tabel_a1_daftar", "Not Found"))
+    print("Database Name:", config.Aliases.get("database"))
+    print("Table A1 Name:", config.Titles.get("tabel_a1_alias_v2"))
+    print("Upload Path for A1:", config.VUploadPath.get("tabel_a1"))
+    print("Flash Message for A1:", config.Views.get("tabel_a1_daftar"))
 
 if __name__ == "__main__":
     main()
